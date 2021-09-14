20fb879834a1854aa68b6ec71ecf5fd2035059be
su <su@DESKTOP-93TC6F5>
2021-09-14 10:34:22 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static double Cos(double x)
+        {
+            const int iterations = 30;
+            var res = 0d;
+            var pow = 1d;
+            var sign = 1;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    res += pow * sign;
+                    sign *= -1;
+                }
+                pow *= x / (i + 1);
+            }
+
+            return Math.Round(res,4);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double result = Math.Round(Math.Cos(60 * Math.PI / 180),4);
+            double resultMy = ConsoleApp2.Program.Cos(60 * Math.PI / 180);
+            Assert.AreEqual(resultMy, result);
+        }
+    }
+}
